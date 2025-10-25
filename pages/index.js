14d279e4743c7085658ccc66597b07f2225c7056
eyadyasser050 <eyadyasser050@gmail.com>
2025-10-25 12:18:54 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      backgroundColor: "#fff",
+      color: "#c00",
+      fontFamily: "sans-serif"
+    }}>
+      <h1 style={{ fontSize: "48px", marginBottom: "10px" }}>Baze</h1>
+      <p style={{ fontSize: "20px" }}>Where basics become bold</p>
+    </div>
+  )
+}
